979425de033f7fa59ccc7329fb60085ef9fc2352
pfr2102 <pfrs2102@gmail.com>
2024-02-22 16:28:31 -0700

    Ranking Votos estable con diseño

---
+++ src/components/Admin/Header/HeaderRankin.jsx
@@ -0,0 +1,88 @@
+import React, { useState ,useEffect } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import {Form, Button, Icon, Dropdown } from 'semantic-ui-react';
+import {useVotes} from '../../../hooks';
+import './HeaderRankin.scss';
+
+export const HeaderRankin = ({getVotesManual}) => {
+
+     /* SECCION DE FUNCIONALIDAD DEL FORMULARIO */
+     const formik = useFormik({
+        initialValues:{
+            id_etapa_fk: '',
+            id_rango_fk: '',
+            fecha_voto: '',
+        },
+        /* esquema de validaciones o restricciones de cada campo */
+        validationSchema: Yup.object({
+            id_etapa_fk: Yup.number().required('El numero de etapa'), 
+            id_rango_fk: Yup.number().required('El numero del rango'), 
+            fecha_voto: Yup.number().required('El año de las votaciones o nominaciones'),                
+        }),
+        /* es la accion a realizar cuando el usuario envia el formulario */
+        validateOnChange: false,
+        onSubmit: async (formValue) => {
+            try {//si hay un usuario actualizamos
+                await getVotesManual(formValue); //llamamos a la api para crear el usuario con una solicitud post 
+            }catch (error) {
+                console.error(error);
+                toast.error(`Error: ${error}`);
+            }
+        }
+    })
+
+
+  return (
+    <>
+        <div className='header-page-admin'>
+            <h2>Consultar ranking de votos</h2>             
+        </div>
+    
+        <div className='add-edit-user-form'>   
+            <Form className='add-edit-user-form' onSubmit={formik.handleSubmit}>
+                {/* Dropdown de Etapas */}
+                <Form.Field>
+                    <label>Etapa</label>
+                    <Dropdown
+                    selection
+                    name='id_etapa_fk'
+                    placeholder='Selecciona una etapa'
+                    options={[
+                        { key: '1', value: '1', text: 'Nominaciones' },
+                        { key: '2', value: '2', text: 'Selecciones' },
+                    ]}
+                    onChange={(e, { value }) => formik.setFieldValue('id_etapa_fk', value)}
+                    value={formik.values.id_etapa_fk}
+                    />
+                </Form.Field>
+                {/* Dropdown de Rangos */}
+                <Form.Field>
+                    <label>Rango</label>
+                    <Dropdown
+                    selection
+                    name='id_rango_fk'
+                    placeholder='Selecciona un rango'
+                    options={[
+                        { key: '1', value: '1', text: 'Superior' },
+                        { key: '2', value: '2', text: 'Medio' },
+                        { key: '3', value: '3', text: 'Operativo' },
+                    ]}
+                    onChange={(e, { value }) => formik.setFieldValue('id_rango_fk', value)}
+                    value={formik.values.id_rango_fk}
+                    />
+                </Form.Field>
+
+                <Form.Input name='fecha_voto' label='Año' placeholder='Año' onChange={formik.handleChange} value={formik.values.fecha_voto} error={formik.errors.fecha_voto} />
+
+                <Button type='submit' primary fluid content='Buscar' />
+            </Form>
+
+            <Button positive>
+            <Icon name='refresh' />
+            </Button>
+        </div>
+        <br />
+    </>
+  )
+}
--- src/pages/Admin/RankingVotes.jsx
+++ src/pages/Admin/RankingVotes.jsx
@@ -1,5 +1,6 @@
 import React, { useState ,useEffect } from 'react'
 import { useVotes } from '../../hooks';
+import { HeaderRankin } from '../../components/Admin/Header/HeaderRankin';
 import { Header, TableVotes, AddEditUserForm } from '../../components/Admin';
 import { ModalBasic } from '../../components/common';
 import { Loader } from 'semantic-ui-react';
@@ -27,57 +28,13 @@ export const RankingVotes = () => {
       /* FUNCIONES PARA LA FUNCIONALIDAD DE LAS VENTANAS */
      const openCloseModal = () => { setShowModal((prev) => !prev); }  // modificar el estado de la ventana (cerrado/abierto) 
      const onRefresh = () => { setRefresh((prev) => !prev);} 
-
-
-     /* SECCION DE FUNCIONALIDAD DEL FORMULARIO */
-     const formik = useFormik({
-        initialValues:{
-            id_etapa_fk: '',
-            id_rango_fk: '',
-            fecha_voto: '',
-        },
-        /* esquema de validaciones o restricciones de cada campo */
-        validationSchema: Yup.object({
-            id_etapa_fk: Yup.number().required('El numero de etapa'), 
-            id_rango_fk: Yup.number().required('El numero del rango'), 
-            fecha_voto: Yup.number().required('El año de las votaciones o nominaciones'),                
-        }),
-        /* es la accion a realizar cuando el usuario envia el formulario */
-        validateOnChange: false,
-        onSubmit: async (formValue) => {
-            try {//si hay un usuario actualizamos
-                await getVotesManual(formValue); //llamamos a la api para crear el usuario con una solicitud post 
-            }catch (error) {
-                console.error(error);
-                toast.error(`Error: ${error}`);
-            }
-        }
-    })
-
+    
 
   return (
     <>
-        <div className='header-page-admin'>
-            <h2>Consultar ranking de votos</h2>
+        <HeaderRankin getVotesManual={getVotesManual}/>
 
-            <div>   
-                <Form className='add-edit-user-form' onSubmit={formik.handleSubmit}>
-                    <Form.Input name='id_etapa_fk' label='Etapa' placeholder='Etapa' onChange={formik.handleChange} value={formik.values.id_etapa_fk} error={formik.errors.id_etapa_fk} />
-                    <Form.Input name='id_rango_fk' label='Rango' placeholder='Rango' onChange={formik.handleChange} value={formik.values.id_rango_fk} error={formik.errors.id_rango_fk} />
-                    <Form.Input name='fecha_voto' label='año' placeholder='año' onChange={formik.handleChange} value={formik.values.fecha_voto} error={formik.errors.fecha_voto} />
-
-                    <Button type='submit' primary fluid content ='Buscar'/>
-                </Form>
-
-                <Button positive>
-                    <Icon name='refresh' />
-                </Button>        
-            </div>
-        </div>
-
-        <br />
-
-        {loading ? (<Loader active inline='centered'> Cargando...</Loader> ) 
+        {loading ? (<Loader active inline='centered'> Esperando Datos...</Loader> ) 
         :  
         (
             <TableVotes votes={votes} />        
